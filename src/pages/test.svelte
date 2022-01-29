9b98bec69ddb53971dbc0ef08fd1c0d54497ab69
Kenramiscal1106 <excaliwyvern@outlook.com>
2022-01-29 18:22:25 +0800

    added test.svelte

---
+++ src/pages/test.svelte
@@ -0,0 +1,3 @@
+<main class="content">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim deserunt nobis rem laudantium pariatur aliquid iste dolores eos accusamus ipsam nulla inventore, dolor similique repudiandae a nihil recusandae, eveniet quis non! Iusto exercitationem sunt sed expedita, omnis cum doloribus libero.</p>
+</main>
