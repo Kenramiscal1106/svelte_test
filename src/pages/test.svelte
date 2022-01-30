8fec992b0b4eae7d3f0adecfd6d6f08f5e336674
Kenramiscal1106 <excaliwyvern@outlook.com>
2022-01-31 07:12:41 +0800

    added some content

--- src/pages/test.svelte
+++ src/pages/test.svelte
@@ -1,12 +1,16 @@
 <script>
   import Extension from "./test_test.svelte";
 </script>
-<main class="content">
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim deserunt nobis rem laudantium pariatur aliquid iste dolores eos accusamus ipsam nulla inventore, dolor similique repudiandae a nihil recusandae, eveniet quis non! Iusto exercitationem sunt sed expedita, omnis cum doloribus libero.</p>
-  <Extension />
+<main class="content-container">
+  <article class="content">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim deserunt nobis rem laudantium pariatur aliquid iste dolores eos accusamus ipsam nulla inventore, dolor similique repudiandae a nihil recusandae, eveniet quis non! Iusto exercitationem sunt sed expedita, omnis cum doloribus libero.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint quaerat saepe nemo tempora quam, id praesentium, voluptates delectus facere at tempore ducimus eveniet porro doloremque dolorem consectetur optio sit, omnis officiis! Cumque praesentium deserunt illum quod facere expedita nobis modi quo reprehenderit? Ea velit perspiciatis beatae, id totam rem nobis. Minus cum tempore porro tenetur accusantium ipsam sequi temporibus nisi! Numquam officia obcaecati in qui facilis dolore voluptates hic ratione itaque earum! Illo corporis commodi, suscipit, hic cumque quasi inventore eos voluptates nulla voluptas doloribus quam nobis. Mollitia eum maxime consequuntur cupiditate eaque suscipit odit assumenda cum molestias ea vero voluptatum nulla facilis, illo ipsa officiis quae numquam nobis atque? Aliquid, eum! Laborum pariatur minima eum quae consequatur voluptas consectetur molestiae adipisci reiciendis deserunt quas commodi a asperiores necessitatibus laboriosam illum soluta modi, reprehenderit facilis repudiandae? Sapiente pariatur ipsa autem, hic voluptate soluta mollitia nemo molestiae, inventore quos consequuntur? Error, dignissimos? Explicabo ipsum inventore soluta veritatis provident aspernatur optio voluptate illum ipsam magnam iure perferendis iusto omnis tempora, nemo quam ab commodi cumque consequuntur odit voluptates dicta. Dolorem explicabo incidunt velit dolore veritatis deserunt quod voluptatibus quibusdam nisi neque. Consectetur aperiam quam dolores quis exercitationem quibusdam. Expedita molestias nihil id.</p>
+    <Extension />
+  </article>
 </main>
 <style>
-  .content {
-    margin-top:calc((1.5rem * 2) + 1ch);
-  }
+  main.content-container {
+    flex:1 1 0;
+    overflow-y:auto;
+  } 
 </style>
