3596e65d6c3f215f2995080309d73ce342c430c7
Kenramiscal1106 <excaliwyvern@outlook.com>
2022-01-31 07:11:55 +0800

    added default value prop to Navigation component added some backup fonts added flex with flex-direction column

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,17 @@
 	import Content from './pages/test.svelte'
 	import Navigation from './pages/navigation.svelte'
 </script>
-<div class="app">
-	<Navigation/>
+<div class="app dark">
+	<Navigation theme={"light"}/>
 	<Content/>
 </div>
 
 <style>
 	.app {
-		font-family: Montserrat;
-		width:100%;
+		font-family: Montserrat, "Segoe UI", sans-serif;
+		width: 100%;
 		height: 100%;
+		display:flex;
+		flex-direction: column;
 	}
 </style>
